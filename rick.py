cc2b2db3407af71fdff57addc42fa9986620c179
*Red <82349700+yawoffeh@users.noreply.github.com>
2022-05-12 15:31:44 +0000

    Create rick.py

---
+++ rick.py
@@ -0,0 +1,55 @@
+'''                                       ``..`.-----....```                                        
+                                      `.-://::/++ossssssso+/::-----.```                             
+                                   `.-://:::+o+:oysyhddhssoooysssssoo/:-.``                         
+                             ``..-::::+oo+oyhhyyhhyddmmmdhddmmmddddddhhyso+:`                       
+                           -:::://::oyhhhhhhdmmdhdmddmmmmdmmmmmddddmmddhhyyso-`                     
+                         `::::////:oddmmmmdmmmmmmmmmmmmmmmmmmdddddhdmmdhyyyyys:`                    
+                         .::::/+ossyhhdmmmmmmmmmmmmmmmdmmmmmmmddddhhdmmddhhhyys`                    
+                      `.-://+ooo+osyhhdmmmmmmmmmmmmmmmmmmmdmmmmmmdddmmmmmmdddhy.                    
+                     `+oo+///oshhdddddmmmmmmmmmmmmmmmmmmmddddddddmmmmmmmmmdddhy-                    
+                    `/+/:/oyhdmmmmmmmmdddmmmmmmmmmmmmmmmmmddmmmmmmmmmmmmmdddhho`                    
+                   .:--/yddddmmmmmmmmmmmdmmmmmmmmmmmmmmmmddddddddddmmmmdddhhho-                     
+                  .--:/ydmmmmmmmmmmmmmmddddmddddddddmddddddddddddddddddddhhs:`                      
+                 .:/ohddmmmmmmmmmmmmmmmmmddddhhhhhhhhhhhhhhhhhhyhhhhhdddhhs.                        
+                .+shddmmmmmmmmmmmmmmmdmmmdddddhhyyyyssoo++++++///////osss++.                        
+                .+o+ddmmmmmmmmmmmmmmdddddddddhyyssoo+////////::::::---::/:/:`                       
+                 -ohddmmmmmmmmmmmmmmmdddddddhysooo++//////////::::::::::::::-                       
+                 -yhdmmmmmmmmmmmmmmddddddddhysoooo+++/////////:::::::::::::::`                      
+                `ohddmmmmmmmmmmmmmddddddddhyysooooo++++////////::::::::::::::.                      
+                :ydddmmmmmmmmmmmmmdddddddhhyysooooooo+++///////::::::::::::::.                      
+               `/yhddmmmmmmmmmmmmmddddddddhhyssoooooo+++////////////:::::::::.                      
+               ./hhdmmmmmmmmmmmmmmmdddddddhhyssooooooo+++///////:::::::::::::.                      
+              `oyhddmmmmmmmmmmmmmmmmmddddhhyssooooooooo+++ooooosssssoo+/:://+/`                     
+              .ohdddmmmmmmmmmmmmmmmmmdddhyyssooooooossssssooo++++oosso+///+oso`                     
+              `+hddmmmmmmmmmmmmmmmmmmdddhyyssooooossssooo++++++oo+/+oo+/:/+o+`                      
+               :hddmmmmmdhssosydddddddddhhyssoooooo+++++++oososso////++///ss.                       
+               `sdddmmmhooosssoosyhhhdhhhyyssoooo++++////////:::::////++///-`                       
+                :ydddmd+oys++syooossyyhhyyssoooooo++++/////:::::::////++++/:.`                      
+                `oyhddh+syo//ohsoooossssssssoooooooo++++//////:::::///+++++/:-`                     
+                `/sydddyoso/ososss+osssssssssooooooooo++++/////:::::/+ooo++//:-                     
+                 `/yhdddyso++++osyooosssssssssssooooooooo+++/////::/+so++++++/-                     
+                  `+yhdddysooo+++osoossssssssssssssoooooo+++//////:/ssoosyyso-                      
+                   .+ydddhysooo+//+oosssssssssssssooooooo+++///////://+//++:`                       
+                    .+hdhyyyyyssooosssssssssssssssooooooo+++/////////:::::/-                        
+                     .ohysssssyyyyssssssssssssssssoooooo++++////////////://`                        
+                     `-+ossssssssssssssssssssssssssoooooo+++///+osooo+++++/`                        
+                      .-/osssssssssssssssssssssssssooooooo++/////+ooooooo+-                         
+                      .-/ossssssssosssssssssssssssssoooooo++//////++++///:                          
+                      `-/osssssssssssssssssssssssssssooooo+++////////////`                          
+                 `:-...-/oossssssssssssssssssssssssssssooooo+////////::::`                          
+                .o-`--.-ooossssssssssssssssssssssssssssssoooo++//////://.                           
+              `-+o`-+/:+sssssssssssssssssssssssssssyyyyssssssooo++/////-`                           
+            `-/ohs..ossssssssssssssssssssssssssssssssyyyyyssssssssoo+/-`                            
+           ./syhdd+.:sssssssssssssssssssssssosooosssssyyyo--...----..`                              
+       ``-/shddmmmh:-/syssssssssssssooooooooooooooossssso/-.                                        
+    `.:/oydmmmmNNNm+--/yyssssssssssoo++++oooooooo+++++////:..`                                      
+..-/oyhdmmNNNNNNNNNy/-./sssssssssssoo++++++oooo+++///////++-.::.`                                   
+shdmmmmmNNNNNNNNNNNm+:-.:osssssssssoo+++++++oooooo+++++//++-.-hdyo/:-```````                        
+mmNNNNNNNNNNNNNNNNNNy/-...+ssssssssoo++++++++++oo+++++//+s:-../mmmyy++ooooooo+:/:                   
+NNNNNNNNNNNNNNNNNNNNmo:-.../ssssssssoo++++++++++++++++/+s+--...dmmhyyyysyyyyyyoso```                
+NNNNNNNNNNNNNNNNNNNNNdo:-...:ossssssoo+++++++++++++++++yy::-...ommyyyyysssssssoso+/-..`             
+NNNNNNNNNNNNNNNNNNNNNNh+:-...-+sssssoo++++++++oo+++++oyh+:-..../mdsyyyyyyysyssso/+mddyo/-...`       
+NNNNNNNNNNNNNNNNNNNNNNdo/:-....:osssso++++++++++++++shds/:-....-mdoyyyyyyyyyssso//mmmmmmdhs+:..``   
+NNNNNNNNNNNNNNNNNNNNNNh+/:--....-+ssso++++++++++++oyhhy+:-......hhoyyyyyyyysssoo+ommmmmmmmmmdhs/-'''
+music = lambda ns: [ ["Never gonna ", "NEVER GONNA "][i%2] + ["give you up", "let you down", "run around \
+and desert you", "make you cry", "say goodbye", "tell a lie and hurt you"][n] for i, n in enumerate(ns) ]
