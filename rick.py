7d8e5c1f0647b3b1367e67fbfb6374c4d2ad844c
*Red <82349700+yawoffeh@users.noreply.github.com>
2022-05-12 15:37:21 +0000

    Update rick.py

--- rick.py
+++ rick.py
@@ -53,3 +53,24 @@ NNNNNNNNNNNNNNNNNNNNNNdo/:-....:osssso++++++++++++++shds/:-....-mdoyyyyyyyyyssso
 NNNNNNNNNNNNNNNNNNNNNNh+/:--....-+ssso++++++++++++oyhhy+:-......hhoyyyyyyyysssoo+ommmmmmmmmmdhs/-'''
 music = lambda ns: [ ["Never gonna ", "NEVER GONNA "][i%2] + ["give you up", "let you down", "run around \
 and desert you", "make you cry", "say goodbye", "tell a lie and hurt you"][n] for i, n in enumerate(ns) ]
+
+#OR
+
+def music(n):
+    lyrics = {0:"give you up",
+              1:"let you down",
+              2:"run around and desert you",
+              3:"make you cry",
+              4:"say goodbye",
+              5:"tell a lie and hurt you"
+              }
+    res = []
+    counter = 0
+    for x in n:
+        if counter%2 != 0:
+            res.append("NEVER GONNA " + lyrics[x])
+        else:
+            res.append("Never gonna " + lyrics[x])
+        counter += 1
+    return res
+        
